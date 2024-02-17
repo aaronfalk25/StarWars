01af25da0652fcd82ed9c596c42bb39423e8984f
Aaron Falk <aaronfalk25@gmail.com>
2024-02-16 23:39:36 -0500

    building search functionality

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,20 +2,23 @@ import './globals.css';
 import React, { useEffect, useState } from 'react';
 import api from './api';
 import {Character} from './interfaces';
+import Search from './components/Search';
 
 const Home: React.FC = () => {
-  let peopleMap: Map<string, string> = new Map();
+  const [peopleMap, setPeopleMap] = useState(new Map<string, string>());
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     const cachedMap = localStorage.getItem('peopleMap');
 
     if (cachedMap) {
-      peopleMap = new Map(JSON.parse(cachedMap));
+      setPeopleMap(new Map(JSON.parse(cachedMap)));
     }
     else {
       let i = 0;
       let data: any;
+      let pMap = new Map<string, string>();
+
       do {
         
         let res: any = await api.get(`/people?page=${++i}`);
@@ -24,23 +27,38 @@ const Home: React.FC = () => {
         console.log(data);
         
         data.results.map((person: Character) => {
-          peopleMap.set(person.name, person.url.split('/').slice(-2,-1).join(''));
+          pMap.set(person.name, person.url.split('/').slice(-2,-1).join(''));
         
         })
   
       } while (data.next && data.next !== null)
 
       localStorage.setItem('peopleMap', JSON.stringify(Array.from(peopleMap.entries())));
+      setPeopleMap(pMap);
     }
 
     console.log(peopleMap);
   }
 
-
+  // Fetch data on component mount
   useEffect(() => {
       fetchData();
       setLoading(false);
   }, []);
+
+  // Handle search
+  const searchCallback = (searchResults: string[]) => {
+    if (searchResults.length === 0) {
+      alert('No results found');
+      return;
+    }
+
+    searchResults.forEach(async (url: string) => {
+      const res = await api.get(`/people/${url}`);
+      const person: Character = res.data;
+      console.log(person);
+    });
+  }
 
 
 
@@ -48,10 +66,14 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center w-full gap-6">
-        <h1> Star Wars Profile Page </h1>
+      <main className="flex min-h-screen flex-col items-center w-full gap-6 mt-4">
+          <h1> Star Wars Character Explorer </h1>
+          <p> Enter a character's name and find additional information on them!</p>
         <div>
-
+          <Search 
+            peopleMap={peopleMap} 
+            searchCallback={searchCallback} 
+          />
         </div>
       </main>
     </>
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-const Search: React.FC = () => {
+interface SearchProps {
+    peopleMap: Map<string, string>
+    searchCallback: (searchResults: string[]) => void
+}
+
+const Search: React.FC<SearchProps> = ({peopleMap, searchCallback}) => {
+    function doSearch(e: React.FormEvent) {
+        e.preventDefault();
+
+        const input = document.getElementById('search') as HTMLInputElement;
+        const searchTerm: string = input.value;
+
+        // Length must be at least 2 to limit too many calls
+        if (searchTerm.length < 2) {
+            alert('Search term must be at least 2 characters');
+            return;
+        }
+
+        // Build array of search results that contains the character's URL identifier
+        const searchResults: string[] = [];
+        peopleMap.forEach((value, key) => {
+            if (key.toLowerCase().includes(searchTerm.toLowerCase())) {
+                searchResults.push(value);
+            }
+        });
+
+        searchCallback(searchResults);
+    }
+    
     return (
-        <div>
-        Search
+        <div className="search-container">
+            <form onSubmit={doSearch}>
+                <div>
+                    <input id="search" type="text" placeholder="Search for a character" />
+                    <button type="submit">Search</button>
+                </div>
+            </form>
         </div>
     )
 }
