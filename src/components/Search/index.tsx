4ffebdc2009eb5e95d7f4cd1dd60829c6a30b675
Aaron Falk <aaronfalk25@gmail.com>
2024-02-18 21:28:36 -0500

    remove unutilized useEffect

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Alert from '../Alert';
