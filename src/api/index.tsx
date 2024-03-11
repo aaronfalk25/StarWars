ec6daf4d7d69133aad99b9674c31d6659b3d3405
Aaron Falk <aaronfalk25@gmail.com>
2024-03-10 23:27:28 -0400

    remove test dir, fix api route (getting 404 on .dev)

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://swapi.dev/api',
+  baseURL: 'https://swapi.py4e.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
